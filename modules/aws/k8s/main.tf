eda38f7d6401ded6adfea9749019097e7e5dbc9e
Sergey Marunich <marunich.s@gmail.com>
2022-06-09 07:31:31 -0400

    egress rules

--- modules/aws/k8s/main.tf
+++ modules/aws/k8s/main.tf
@@ -26,6 +26,17 @@ module "eks" {
     }
   }
 
+  cluster_security_group_additional_rules = {
+    egress_nodes_ephemeral_ports_tcp = {
+      description                = "To node 1025-65535"
+      protocol                   = "tcp"
+      from_port                  = 1025
+      to_port                    = 65535
+      type                       = "egress"
+      source_node_security_group = true
+    }
+  }
+
   node_security_group_additional_rules = {
     ingress_self_all = {
       description = "Node to node all ports/protocols"
