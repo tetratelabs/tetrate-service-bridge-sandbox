f51d3757d5284b5a13461a373b4acaef3eef202e
Sergey Marunich <marunich.s@gmail.com>
2022-07-28 11:57:50 -0400

    add cert-manager

--- infra/azure/main.tf
+++ infra/azure/main.tf
@@ -44,9 +44,9 @@ module "azure_k8s" {
 
 module "cert-manager" {
   source                     = "../../modules/addons/cert-manager"
-  cluster_name               = module.aws_k8s[0].cluster_name
-  k8s_host                   = module.aws_k8s[0].host
-  k8s_cluster_ca_certificate = module.aws_k8s[0].cluster_ca_certificate
-  k8s_client_token           = module.aws_k8s[0].token
+  cluster_name               = module.azure_k8s[0].cluster_name
+  k8s_host                   = module.azure_k8s[0].host
+  k8s_cluster_ca_certificate = module.azure_k8s[0].cluster_ca_certificate
+  k8s_client_token           = module.azure_k8s[0].token
   cert-manager_enabled       = var.cert-manager_enabled
 }
