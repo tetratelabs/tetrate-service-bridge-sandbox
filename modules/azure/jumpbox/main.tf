5df62edc2ee11089e1301b1efb464cd17e802f3e
Sergey Marunich <marunich.s@gmail.com>
2022-11-01 19:12:44 -0400

    uplift aws and azure jumpbox to 22.04, addressing @nacx comments

--- modules/azure/jumpbox/main.tf
+++ modules/azure/jumpbox/main.tf
@@ -120,7 +120,7 @@ resource "azurerm_linux_virtual_machine" "jumpbox" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22.04-LTS"
     version   = "latest"
   }
 
