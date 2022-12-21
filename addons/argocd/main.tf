a587a726669bd1cbaf3003a6c628362eab003676
Ignasi Barrera <ignasi@tetrate.io>
2022-12-21 22:44:31 +0100

    Move argocd apps out of the generic module

--- addons/argocd/main.tf
+++ addons/argocd/main.tf
@@ -12,4 +12,6 @@ module "argocd" {
   k8s_cluster_ca_certificate = data.terraform_remote_state.infra.outputs.cluster_ca_certificate
   k8s_client_token           = data.terraform_remote_state.infra.outputs.token
   password                   = var.tsb_password
+
+  applications = {for a in fileset("${path.module}/applications", "*.yaml") : a => file("${path.module}/applications/${a}")}
 }
