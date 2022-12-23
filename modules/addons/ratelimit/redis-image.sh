1a11b535e1dcc95c2997d07bd476cef4240e9333
Ignasi Barrera <ignasi@tetrate.io>
2022-12-24 00:24:07 +0100

    proper tmp dir

--- modules/addons/ratelimit/redis-image.sh
+++ modules/addons/ratelimit/redis-image.sh
@@ -4,9 +4,9 @@ set -e
 
 eval "$(jq -r '@sh "TSB_VERSION=\(.tsb_version) REGISTRY=\(.registry)"')"
 
-rm -rf /tmp/controlplane
-helm pull tetrate-tsb-helm/controlplane --version "${TSB_VERSION}" --untar --untardir /tmp
-REDIS_IMAGE=$(grep redis /tmp/controlplane/images.txt)
+TMP_DIR=$(mktemp -d)
+helm pull tetrate-tsb-helm/controlplane --version "${TSB_VERSION}" --untar --untardir "${TMP_DIR}"
+REDIS_IMAGE=$(grep redis "${TMP_DIR}/controlplane/images.txt")
 REDIS_IMAGE=${REDIS_IMAGE/containers.dl.tetrate.io/${REGISTRY}}
 
 echo "{\"image\":\"${REDIS_IMAGE}\"}"
