a054a6894cd26cec727d635da849ce4aa82f2547
Ignasi Barrera <ignasi@tetrate.io>
2023-01-20 15:29:18 +0100

    move commoon values out of the provider-specific values

--- modules/addons/external-dns/main.tf
+++ modules/addons/external-dns/main.tf
@@ -14,9 +14,35 @@ resource "helm_release" "external_dns" {
   namespace        = "external-dns"
   timeout          = 900
 
+  set {
+    name  = "policy"
+    value = "upsert-only"
+  }
+
+  set {
+    name  = "registry"
+    value = "txt"
+  }
+
+  set {
+    name  = "txtOwnerId"
+    value = var.cluster_name
+  }
+
+  # Since we are enabling the Istio Gateway source, it is important to set this filter
+  # so we don't create DNS records for any random or internal hostnames defined in
+  # the TSB IngressGateways.
+  set {
+    name  = "domainFilters"
+    value = "{${var.dns_zone}}"
+  }
+
+  set {
+    name  = "sources"
+    value = "{service,ingress,istio-gateway}"
+  }
+
   values = [templatefile("${path.module}/manifests/values-${var.dns_provider}.yaml.tmpl", {
-    dns_zone                   = var.dns_zone
-    dns_owner_id               = var.cluster_name
     google_project             = var.google_project
     google_service_account_key = indent(4, var.google_service_account_key)
   })]
