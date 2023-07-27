cf1e2fca8481a117753e0c09c3df94320d02da19
Sergey Marunich <marunich.s@gmail.com>
2023-07-27 14:56:42 -0400

    AWS tflinting

--- infra/aws/k8s_auth/main.tf
+++ infra/aws/k8s_auth/main.tf
@@ -7,6 +7,5 @@ data "terraform_remote_state" "infra" {
 
 module "aws_k8s_auth" {
   source       = "../../../modules/aws/k8s_auth"
-  count        = var.aws_k8s_region == null ? 0 : 1
   cluster_name = data.terraform_remote_state.infra.outputs.cluster_name
 }
--- infra/aws/k8s_auth/outputs.tf
+++ infra/aws/k8s_auth/outputs.tf
@@ -1,4 +1,4 @@
 output "token" {
-  value     = module.aws_k8s_auth[0].token
+  value     = module.aws_k8s_auth.token
   sensitive = true
 }
