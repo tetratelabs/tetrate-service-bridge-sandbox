568d4d12687e6f4371f82daaee839833a8aa1a31
Sergey Marunich <marunich.s@gmail.com>
2023-07-27 14:39:06 -0400

    manually tflinting AWS

--- infra/aws/k8s_auth/variables.tf
+++ infra/aws/k8s_auth/variables.tf
@@ -1,12 +1,15 @@
 variable "cloud" {
+  type    = string
   default = "aws"
 }
 
 variable "cluster_id" {
+  type    = string
   default = null
 }
 
 variable "aws_k8s_region" {
+  type    = list(any)
   default = []
 }
 
--- infra/aws/variables.tf
+++ infra/aws/variables.tf
@@ -1,62 +1,84 @@
 variable "cluster_id" {
+  type    = string
   default = null
 }
 variable "cluster_name" {
+  type    = string
   default = null
 }
 
 variable "name_prefix" {
+  type        = string
   description = "name prefix"
 }
 
 variable "cidr" {
+  type        = string
   description = "cidr"
   default     = "172.20.0.0/16"
 }
 
 variable "tsb_image_sync_username" {
+  type = string
 }
 
 variable "tsb_image_sync_apikey" {
+  type = string
 }
 
 variable "tsb_version" {
+  type    = string
   default = "1.7.0"
 }
 
 variable "tsb_helm_repository" {
+  type    = string
   default = "https://charts.dl.tetrate.io/public/helm/charts/"
 }
 
 variable "jumpbox_username" {
+  type    = string
   default = "tsbadmin"
 }
 
 variable "aws_k8s_region" {
+  type    = string
   default = null
 }
 
 variable "aws_eks_k8s_version" {
+  type    = string
   default = "1.26"
 }
 
 variable "output_path" {
+  type    = string
   default = "../../outputs"
 }
 
 variable "tetrate_owner" {
+  type = string
 }
+
 variable "tetrate_team" {
+  type = string
 }
+
 variable "tetrate_purpose" {
+  type    = string
   default = "demo"
 }
+
 variable "tetrate_lifespan" {
+  type    = string
   default = "oneoff"
 }
+
 variable "tetrate_customer" {
+  type    = string
   default = "internal"
 }
+
 locals {
   default_tags = {
     "tetrate:owner"    = coalesce(var.tetrate_owner, replace(var.tsb_image_sync_username, "/\\W+/", "-"))
