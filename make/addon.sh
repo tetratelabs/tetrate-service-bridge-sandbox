24b44f2d69b887f1d3be26002e6d77f32d66cfdc
Sergey Marunich <marunich.s@gmail.com>
2023-11-07 17:04:56 -0500

    Revert "Re-reading shell scripts"
    This reverts commit 53ffd6ee6231a3fa4d62a78b970d8c98706a5520.

--- make/addon.sh
+++ make/addon.sh
@@ -11,38 +11,49 @@ source "${BASE_DIR}/variables.sh"
 
 ACTION=${1}
 SUPPORTED_CLOUDS=("azure" "aws" "gcp")
-SUPPORTED_ADDONS=("argocd" "fluxcd" "external-dns" "tsb_monitoring")
-
-# Generate SUPPORTED_ACTIONS based on patterns.
-SUPPORTED_ACTIONS=("help")
-for addon in "${SUPPORTED_ADDONS[@]::3}"; do
-  for cloud in "${SUPPORTED_CLOUDS[@]}"; do
-    SUPPORTED_ACTIONS+=("${addon}_${cloud}" "destroy_${addon}_${cloud}")
-  done
-done
+SUPPORTED_REGIONAL_ADDONS=("argocd" "fluxcd" "external-dns")
+SUPPORTED_MP_ADDONS=("tsb-monitoring")
 
 # Validate input values.
-if ! [[ " ${SUPPORTED_ACTIONS[*]} " =~ " ${ACTION} " ]]; then
-  echo "Invalid action '${ACTION}'. Must be one of ${SUPPORTED_ACTIONS[*]}."
+SUPPORTED_ACTIONS=("help"
+                   "argocd_aws" "argocd_azure" "argocd_gcp"
+                   "fluxcd_aws" "fluxcd_azure" "fluxcd_gcp" 
+                   "external_dns_aws" "external_dns_azure" "external_dns_gcp" 
+                   "tsb_monitoring"
+                   "destroy_argocd_aws" "destroy_argocd_azure" "destroy_argocd_gcp"
+                   "destroy_fluxcd_aws" "destroy_fluxcd_azure" "destroy_fluxcd_gcp"
+                   "destroy_external_dns_aws" "destroy_external_dns_azure" "destroy_external_dns_gcp"
+                   "destroy_tsb_monitoring")
+if ! [[ " ${SUPPORTED_ACTIONS[*]} " == *" ${ACTION} "* ]]; then
+  print_error "Invalid action '${ACTION}'. Must be one of '${SUPPORTED_ACTIONS[*]}'."
   exit 1
 fi
 
 # This function provides help information for the script.
-help() {
+function help() {
   echo "Usage: $0 <command> [options]"
   echo "Commands:"
   echo "  help                            Display this help message."
-  for addon in "${SUPPORTED_ADDONS[@]}"; do
-    if [[ " ${SUPPORTED_CLOUDS[*]} " =~ " ${addon} " ]]; then
-      for cloud in "${SUPPORTED_CLOUDS[@]}"; do
-        echo "  ${addon}_${cloud}                    Deploy addon ${addon} on ${cloud}."
-        echo "  destroy_${addon}_${cloud}            Destroy addon ${addon} on ${cloud}."
-      done
-    else
-      echo "  ${addon}                        Deploy addon ${addon}."
-      echo "  destroy_${addon}                Destroy addon ${addon}."
-    fi
-  done
+  echo "  argocd_aws                      Deploy addon argocd on aws."
+  echo "  argocd_azure                    Deploy addon argocd on azure."
+  echo "  argocd_gcp                      Deploy addon argocd on gcp."
+  echo "  fluxcd_aws                      Deploy addon fluxcd on aws."
+  echo "  fluxcd_azure                    Deploy addon fluxcd on azure."
+  echo "  fluxcd_gcp                      Deploy addon fluxcd on gcp."
+  echo "  tsb_monitoring                  Deploy addon tsb_monitoring."
+  echo "  external_dns_aws                Deploy addon external_dns on aws."
+  echo "  external_dns_azure              Deploy addon external_dns on azure."
+  echo "  external_dns_gcp                Deploy addon external_dns on gcp."
+  echo "  destroy_argocd_aws              Destroy addon argocd on aws."
+  echo "  destroy_argocd_azure            Destroy addon argocd on azure."
+  echo "  destroy_argocd_gcp              Destroy addon argocd on gcp."
+  echo "  destroy_fluxcd_aws              Destroy addon fluxcd on aws."
+  echo "  destroy_fluxcd_azure            Destroy addon fluxcd on azure."
+  echo "  destroy_fluxcd_gcp              Destroy addon fluxcd on gcp."
+  echo "  destroy_tsb_monitoring          Destroy addon tsb_monitoring."
+  echo "  destroy_external_dns_aws        Destroy addon external_dns on aws."
+  echo "  destroy_external_dns_azure      Destroy addon external_dns on azure."
+  echo "  destroy_external_dns_gcp        Destroy addon external_dns on gcp."
 }
 
 # This function deploys the specified addon on the specified cloud provider per region.
@@ -205,36 +216,89 @@ function destroy_addon_mp_cluster() {
 #
 # Main execution.
 #
-
-# Switch case to handle actions
 case "${ACTION}" in
   help)
     help
     ;;
-  argocd_aws | argocd_azure | argocd_gcp | \
-  fluxcd_aws | fluxcd_azure | fluxcd_gcp | \
-  external_dns_aws | external_dns_azure | external_dns_gcp)
-    CLOUD="${ACTION##*_}"
-    ADDON="${ACTION%_*}"
-    print_stage "Going to deploy addon $ADDON on cloud $CLOUD"
-    deploy_addon_per_region "$CLOUD" "$ADDON"
+  argocd_aws)
+    print_stage "Going to deploy addon 'argocd' on cloud 'aws'" 
+    deploy_addon_per_region "aws" "argocd"
     ;;
-  destroy_argocd_aws | destroy_argocd_azure | destroy_argocd_gcp | \
-  destroy_fluxcd_aws | destroy_fluxcd_azure | destroy_fluxcd_gcp | \
-  destroy_external_dns_aws | destroy_external_dns_azure | destroy_external_dns_gcp)
-    CLOUD="${ACTION##*_}"
-    ADDON="destroy_${ACTION%_*}"
-    ADDON="${ADDON#destroy_}"
-    print_stage "Going to destroy addon $ADDON on cloud $CLOUD"
-    destroy_addon_per_region "$CLOUD" "$ADDON"
+  argocd_azure)
+    print_stage "Going to deploy addon 'argocd' on cloud 'azure'" 
+    deploy_addon_per_region "azure" "argocd"
+    ;;
+  argocd_gcp)
+    print_stage "Going to deploy addon 'argocd' on cloud 'gcp'" 
+    deploy_addon_per_region "gcp" "argocd"
+    ;;
+  fluxcd_aws)
+    print_stage "Going to deploy addon 'fluxcd' on cloud 'aws'" 
+    deploy_addon_per_region "aws" "fluxcd"
+    ;;
+  fluxcd_azure)
+    print_stage "Going to deploy addon 'fluxcd' on cloud 'azure'" 
+    deploy_addon_per_region "azure" "fluxcd"
+    ;;
+  fluxcd_gcp)
+    print_stage "Going to deploy addon 'fluxcd' on cloud 'gcp'" 
+    deploy_addon_per_region "gcp" "fluxcd"
     ;;
   tsb_monitoring)
-    print_stage "Going to deploy addon 'tsb-monitoring'"
+    print_stage "Going to deploy addon 'tsb-monitoring'" 
     deploy_addon_mp_cluster "tsb-monitoring"
     ;;
+  external_dns_aws)
+    print_stage "Going to deploy addon 'external-dns' on cloud 'aws'" 
+    deploy_addon_per_region "aws" "external-dns"
+    ;;
+  external_dns_azure)
+    print_stage "Going to deploy addon 'external-dns' on cloud 'azure'" 
+    deploy_addon_per_region "azure" "external-dns"
+    ;;
+  external_dns_gcp)
+    print_stage "Going to deploy addon 'external-dns' on cloud 'gcp'" 
+    deploy_addon_per_region "gcp" "external-dns"
+    ;;
+  destroy_argocd_aws)
+    print_stage "Going to destroy addon 'argocd' on cloud 'aws'" 
+    destroy_addon_per_region "aws" "argocd"
+    ;;
+  destroy_argocd_azure)
+    print_stage "Going to destroy addon 'argocd' on cloud 'azure'" 
+    destroy_addon_per_region "azure" "argocd"
+    ;;
+  destroy_argocd_gcp)
+    print_stage "Going to destroy addon 'argocd' on cloud 'gcp'" 
+    destroy_addon_per_region "gcp" "argocd"
+    ;;
+  destroy_fluxcd_aws)
+    print_stage "Going to destroy addon 'fluxcd' on cloud 'aws'" 
+    destroy_addon_per_region "aws" "fluxcd"
+    ;;
+  destroy_fluxcd_azure)
+    print_stage "Going to destroy addon 'fluxcd' on cloud 'azure'" 
+    destroy_addon_per_region "azure" "fluxcd"
+    ;;
+  destroy_fluxcd_gcp)
+    print_stage "Going to destroy addon 'fluxcd' on cloud 'gcp'" 
+    destroy_addon_per_region "gcp" "fluxcd"
+    ;;
   destroy_tsb_monitoring)
-    print_stage "Going to destroy addon 'tsb-monitoring'"
+    print_stage "Going to destroy addon 'tsb-monitoring'" 
     destroy_addon_mp_cluster "tsb-monitoring"
+    ;;
+  destroy_external_dns_aws)
+    print_stage "Going to destroy addon 'external-dns' on cloud 'aws'" 
+    destroy_addon_per_region "aws" "external-dns"
+    ;;
+  destroy_external_dns_azure)
+    print_stage "Going to destroy addon 'external-dns' on cloud 'azure'" 
+    destroy_addon_per_region "azure" "external-dns"
+    ;;
+  destroy_external_dns_gcp)
+    print_stage "Going to destroy addon 'external-dns' on cloud 'gcp'" 
+    destroy_addon_per_region "gcp" "external-dns"
     ;;
   *)
     print_error "Invalid option. Use 'help' to see available commands."
