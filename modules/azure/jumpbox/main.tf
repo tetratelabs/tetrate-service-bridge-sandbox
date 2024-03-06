tetratelabs/tetrate-service-bridge-sandbox

commit 13dcc403454024fd16fc2044c9d6a61caba0bdb7

Development build | update TF modules and helm vars (#357)
@smarunich @shamusx PR is ready to be reviewed. I have tested the
changes only in GCP, and not tested specifically for AWS and Azure.

modules/azure/jumpbox/main.tf
@@ -117,6 +117,8 @@ resource "azurerm_linux_virtual_machine" "jumpbox" {
     jumpbox_username          = var.jumpbox_username
     tsb_version               = var.tsb_version
     tsb_image_sync_username   = var.tsb_image_sync_username
+    tsb_helm_username         = var.helm_username
+    tsb_helm_password         = var.helm_password
     tsb_image_sync_apikey     = var.tsb_image_sync_apikey
     docker_login              = "docker login -u ${var.registry_username} -p ${var.registry_password} ${var.registry}"
     registry                  = var.registry

infra/aws/main.tf
@@ -23,6 +23,8 @@ module "aws_jumpbox" {
   cidr                    = module.aws_base.cidr
   tsb_version             = local.tetrate.version
   tsb_helm_repository     = local.tetrate.helm_repository
+  tsb_helm_username       = local.tetrate.helm_username
+  tsb_helm_password       = local.tetrate.helm_password
   jumpbox_username        = var.jumpbox_username
   machine_type            = var.jumpbox_machine_type
   tsb_image_sync_username = local.tetrate.image_sync_username

infra/azure/main.tf
@@ -15,6 +15,8 @@ module "azure_jumpbox" {
   vnet_subnet             = module.azure_base.vnet_subnets[0]
   tsb_version             = local.tetrate.version
   tsb_helm_repository     = local.tetrate.helm_repository
+  tsb_helm_username       = local.tetrate.helm_username
+  tsb_helm_password       = local.tetrate.helm_password
   jumpbox_username        = var.jumpbox_username
   machine_type            = var.jumpbox_machine_type
   tsb_image_sync_username = local.tetrate.image_sync_username

infra/gcp/main.tf
@@ -35,6 +35,8 @@ module "gcp_jumpbox" {
   vpc_subnet              = module.gcp_base.vpc_subnets[0]
   tsb_version             = local.tetrate.version
   tsb_helm_repository     = local.tetrate.helm_repository
+  tsb_helm_username       = local.tetrate.helm_username
+  tsb_helm_password       = local.tetrate.helm_password
   jumpbox_username        = var.jumpbox_username
   machine_type            = var.jumpbox_machine_type
   tsb_image_sync_username = local.tetrate.image_sync_username

modules/aws/jumpbox/main.tf
@@ -263,6 +263,8 @@ resource "aws_instance" "jumpbox" {
   user_data = base64encode(templatefile("${path.module}/jumpbox.userdata", {
     jumpbox_username          = var.jumpbox_username
     tsb_version               = var.tsb_version
+    tsb_helm_username         = var.tsb_helm_username
+    tsb_helm_password         = var.tsb_helm_password
     tsb_image_sync_username   = var.tsb_image_sync_username
     tsb_image_sync_apikey     = var.tsb_image_sync_apikey
     docker_login              = "aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.registry}"

modules/gcp/jumpbox/main.tf
@@ -71,6 +71,8 @@ resource "google_compute_instance" "jumpbox" {
       registry                  = var.registry
       pubkey                    = tls_private_key.generated.public_key_openssh
       tsb_helm_repository       = var.tsb_helm_repository
+      tsb_helm_username         = var.tsb_helm_username
+      tsb_helm_password         = var.tsb_helm_password
       tetrate_internal_cr       = module.internal_registry.internal_cr
       tetrate_internal_cr_token = module.internal_registry.internal_cr_token
     })

modules/aws/jumpbox/jumpbox.userdata
@@ -32,7 +32,7 @@ write_files:
       ${docker_login}
       if [[ ! -z "${tetrate_internal_cr}" ]]; then
         curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-        helm repo add tetrate-tsb-helm "${tsb_helm_repository}"
+        helm repo add tetrate-tsb-helm "${tsb_helm_repository}"  --username "${tsb_helm_username}" --password "${tsb_helm_password}"
         helm repo update
         curl -Lo "/usr/local/bin/tctl" "https://binaries.dl.tetrate.io/public/raw/versions/linux-amd64-next/tctl"
         chmod +x "/usr/local/bin/tctl"

modules/aws/jumpbox/variables.tf
@@ -51,6 +51,16 @@ variable "tsb_helm_repository" {
   type = string
 }
 
+variable "tsb_helm_username" {
+  type    = string
+  default = ""
+}
+
+variable "tsb_helm_password" {
+  type    = string
+  default = ""
+}
+
 variable "output_path" {
   type = string
 }

commit 4ef2a568b6c6d3a43b3d444c7335eba0cf7ee328

fixing typo introduced by https://github.com/tetrateio/tetrate-service-bridge-sandbox/pull/357
fixing typo introduced by https://github.com/tetrateio/tetrate-service-bridge-sandbox/pull/357

modules/azure/jumpbox/main.tf
@@ -117,8 +117,8 @@ resource "azurerm_linux_virtual_machine" "jumpbox" {
     jumpbox_username          = var.jumpbox_username
     tsb_version               = var.tsb_version
     tsb_image_sync_username   = var.tsb_image_sync_username
-    tsb_helm_username         = var.helm_username
-    tsb_helm_password         = var.helm_password
+    tsb_helm_username         = var.tsb_helm_username
+    tsb_helm_password         = var.tsb_helm_password
     tsb_image_sync_apikey     = var.tsb_image_sync_apikey
     docker_login              = "docker login -u ${var.registry_username} -p ${var.registry_password} ${var.registry}"
     registry                  = var.registry