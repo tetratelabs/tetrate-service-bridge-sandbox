5437e3199cc8f27268e94801780cf6e83f9096c9
Sergey Marunich <marunich.s@gmail.com>
2024-04-30 08:20:11 -0400

    Update main.tf

--- modules/gcp/k8s_auth/main.tf
+++ modules/gcp/k8s_auth/main.tf
@@ -1,9 +1,14 @@
+data "google_compute_zones" "available" {
+  project = var.project_id
+  region  = var.region
+}
+
 module "gke_auth" {
   source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
 
   project_id           = var.project_id
   cluster_name         = var.cluster_name
-  location             = var.region
+  location             = data.google_compute_zones.available.names[0]
   use_private_endpoint = false
 
 }
